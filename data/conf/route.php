d22d89b10f98e738bf616c91432ece28add857f4
JellyChen <451453325@qq.com>
2018-02-07 16:16:08 +0800

    init thinkcmf

--- data/conf/route.php
+++ data/conf/route.php
@@ -1,2 +1,25 @@
 <?php	return array (
+  'ewfgt/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'ewfgt' => 
+  array (
+    0 => 'portal/List/index?id=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
 );
