5ae22aada6ae40f8b60ec048b993a44fb2ac6665
jellychen <451453325@qq.com>
2018-02-27 23:40:55 +0800

    添加订单表

---
+++ app/common/model/OrderModel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * FileName:OrderModel.php
+ * Created by JellyChen
+ * Date: 2018/2/27 下午9:38
+ *
+ * @author         JellyChen
+ * @copyright      All Rights Reserved.
+ * @version        1.0
+ */
+
+namespace app\common\model;
+
+
+class OrderModel extends BaseModel {
+    protected $table = 'hair_order';
+
+}
