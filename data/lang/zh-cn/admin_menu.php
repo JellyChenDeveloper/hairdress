7e1d22d4623fdf9e2891e612ddcbecbe7f8804be
JellyChen <451453325@qq.com>
2018-03-09 16:35:24 +0800

    修改后台目录

--- data/lang/zh-cn/admin_menu.php
+++ data/lang/zh-cn/admin_menu.php
@@ -3,4 +3,6 @@ return array (
   'ADMIN_ORDER_INDEX' => '订单管理',
   'ADMIN_USER_INDEX' => '管理员',
   'ADMIN_WXUSERINFO_INDEX' => '微信用户',
+  'PORTAL_ADMININDEX_DEFAULT' => '文章管理',
+  'PORTAL_ADMININDEX_INDEX' => '文章列表',
 );
