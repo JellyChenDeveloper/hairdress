82c1656ef29ffe1b7209cd409f625496ce31bb1b
jellychen <451453325@qq.com>
2018-03-13 21:47:47 +0800

    邀请码绑定操作

--- app/hair/controller/IndexController.php
+++ app/hair/controller/IndexController.php
@@ -24,19 +24,40 @@ class IndexController extends HairBaseController {
     }
 
     public function doRegister() {
-        $from_url       = session('register.from_url');
+        $from_url = session('register.from_url');
+        $user_id  = $this->request->post('user_id');
+        if ($user_id != $this->user_id) {
+            $this->error(lang('页参数错误，请刷新重试'));
+        }
+
+        $data = $this->request->param();
+
+        $result = $this->validate($data, [
+            'user_id'        => 'require',
+            'mobile'         => 'require',
+            'activation_key' => 'require|length:8',
+        ], [
+            'user_id.require'        => '参数错误，请刷新重试',
+            'mobile.require'         => '手机号不能为空',
+            'activation_key.require' => '邀请码不能为空',
+            'activation_key.length'  => '邀请码必须为8位长度',
+        ]);
+        if ($result !== true) {
+            $this->error($result);
+        }
+
         $mobile         = $this->request->post('mobile');
         $activation_key = $this->request->post('activation_key');
-        $user_id        = $this->request->post('user_id');
-        if ($user_id != $this->user_id) {
-            $this->error(lang('页面错误，请关闭页面后重试!'));
-        }
 
         $data = [
             'id'             => $user_id,
             'mobile'         => $mobile,
             'activation_key' => $activation_key,
         ];
+        $code = model('ActivityCode')->get(['code' => $activation_key]);
+        if (!$code) {
+            $this->error('邀请码不存在，请重新输入');
+        }
         $user = model('WechatUser')->get($user_id);
         if (!$user) {
             $this->error('用户不存在');
@@ -45,6 +66,7 @@ class IndexController extends HairBaseController {
         $result = $user->data($data)->save();
         cmf_update_current_user(model('WechatUser')->get($user_id)->toArray());
         if ($result == 1) {
+            $code->setInc('count');
             $this->success(lang('注册成功'), null, ['from_url' => $from_url]);
         } else {
             $this->error(lang('该用户已注册'));
