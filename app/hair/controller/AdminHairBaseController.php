5e708123b5bc15dedf0ec4e40e1d24d95e194e02
jellychen <451453325@qq.com>
2018-03-22 22:59:50 +0800

    添加后端控制器基类

---
+++ app/hair/controller/AdminHairBaseController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * FileName:AdminHairBaseController.php
+ * Created by JellyChen
+ * Date: 2018/3/22 下午10:58
+ *
+ * @author         JellyChen
+ * @copyright      All Rights Reserved.
+ * @version        1.0
+ */
+
+namespace app\hair\controller;
+
+use cmf\controller\AdminBaseController;
+
+class AdminHairBaseController extends AdminBaseController {
+
+}
