712169f7a6f02aca5887e748b217655117b7d124
jellychen <451453325@qq.com>
2018-04-15 17:02:40 +0800

    功能组件添加锁定和支付的功能

--- docker/sql/table_hair_element.sql
+++ docker/sql/table_hair_element.sql
@@ -9,7 +9,6 @@ CREATE TABLE `hair_element` (
   `thumb` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '组件缩略图',
   `type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '组件类型:0-头像,1-工具,2-功能组件',
   `is_lock` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否锁定:0-否,1-是',
-  `is_free` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否免费:0-否,1-是',
   `price` float NOT NULL DEFAULT '0' COMMENT '价格',
   `list_order` int(10) unsigned NOT NULL DEFAULT '10000' COMMENT '排序，0-10000，从小到大',
   `status` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '状态:0:已隐藏,1:正常',
@@ -19,5 +18,15 @@ CREATE TABLE `hair_element` (
   `delete_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '删除时间',
   PRIMARY KEY (`id`),
   KEY `alias` (`alias`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='组件表';
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COMMENT='组件表';
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hair_element`
+--
+
+LOCK TABLES `hair_element` WRITE;
+/*!40000 ALTER TABLE `hair_element` DISABLE KEYS */;
+INSERT INTO `hair_element` VALUES (1,'画笔','drawpanel','','',2,0,0,10000,1,NULL,1523780329,1523781457,0),(2,'发片','hairpiece','','',2,0,0,10000,1,NULL,1523780329,1523782221,0),(3,'发片2','hairpiece2','','',2,0,0,10000,1,NULL,1523780329,1523780329,0),(4,'分区线','partline','','',2,0,0,10000,1,NULL,1523780329,1523780329,0),(5,'弧线','arc','','',2,0,0,10000,1,NULL,1523780329,1523780329,0),(6,'直线','line','','',2,0,0,10000,1,NULL,1523780329,1523780329,0),(7,'S线','Sline','','',2,0,0,10000,1,NULL,1523780329,1523780329,0),(8,'S虚线','SIline','','',2,0,0,10000,1,NULL,1523780329,1523780329,0),(9,'箭头','arrow','','',2,0,0,10000,1,NULL,1523780329,1523780329,0),(10,'虚箭头','iarrow','','',2,0,0,10000,1,NULL,1523780329,1523780329,0),(11,'圆形','circle','','',2,0,0,10000,1,NULL,1523780329,1523780329,0),(12,'正方形','square','','',2,0,0,10000,1,NULL,1523780329,1523780329,0),(13,'三角形','triangle','','',2,0,0,10000,1,NULL,1523780329,1523780329,0);
+/*!40000 ALTER TABLE `hair_element` ENABLE KEYS */;
+UNLOCK TABLES;
