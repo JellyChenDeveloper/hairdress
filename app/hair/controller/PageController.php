fc0920590701e16a1c6157118574a3171763c4ac
JellyChen <451453325@qq.com>
2018-04-24 10:17:24 +0800

    添加测试页面

--- app/hair/controller/PageController.php
+++ app/hair/controller/PageController.php
@@ -59,6 +59,56 @@ class PageController extends HairBaseController {
 
         return $this->fetch();
     }
+    public function index1($id) {
+        $page = model('Pages')->get(['id' => $id, 'user_id' => $this->user_id]);
+        $this->assign('page', $page);
+
+        $elements = model('Element')->all(['type' => ['in', [0, 1, 2]]])->toArray();
+        $orders   = model('Order')->where(['user_id' => $this->user_id, 'type' => 1, 'pay_time' => ['gt', 0]])->column('element_id');
+        $heads    = [];
+        $tools    = [];
+        $funcs    = [];
+
+        foreach ($elements as $k => $v) {
+            $v['need_pay'] = 0;
+            if ($v['price'] != 0 && !in_array($v['id'], $orders)) {
+                $v['need_pay'] = 1;
+            }
+            switch ($v['type']) {
+                case 1:
+                    $tools[$v['id']] = $v;
+                    break;
+                case 2:
+                    $funcs[$v['alias']] = $v;
+                    break;
+                case 0:
+                default:
+                    $heads[$v['id']] = $v;
+                    break;
+            }
+        }
+        $this->assign('heads', $heads);
+        $this->assign('tools', $tools);
+        $this->assign('funcs', $funcs);
+
+        $js_config = $this->wecharService->jssdk()->buildConfig([
+            "onMenuShareTimeline",
+            "onMenuShareAppMessage",
+            "onMenuShareQQ",
+            "onMenuShareWeibo",
+        ]);
+        $share     = [
+            'js_config'   => $js_config,
+            'share_title' => $this->setting['software_name'],
+            'share_desc'  => '分享描述',
+            'share_link'  => url('hair/index/index', '', true, true),
+            'share_img'   => $page->avatar_url,
+        ];
+        $this->assign('share', $share);
+        session('wx_pay.from_url', $this->request->url(true));
+
+        return $this->fetch();
+    }
 
     public function doAddPage() {
         $page_name  = $this->request->post('page_name');
